12a0eea0d6c0b35abdb105c21a26329a041323d0
MEHUL BHARDWAJ <66671284+mehulbhardwaj1609@users.noreply.github.com>
2021-09-15 23:32:02 +0530

    added board state and also react events

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,8 +3,22 @@ import Square from './Square';
 
 const Board = () => {
   const [board, setBoard] = useState(Array(9).fill(null));
+  const [isNext, setIsNext] = useState(false);
 
-  const handleSquareClick = () => {};
+  const handleSquareClick = position => {
+    if (board[position]) return;
+
+    setBoard(prev => {
+      return prev.map((square, pos) => {
+        if (pos === position) {
+          return isNext ? 'X' : 'O';
+        }
+
+        return square;
+      });
+    });
+    setIsNext(prev => !prev);
+  };
 
   const renderSquare = position => {
     return (
