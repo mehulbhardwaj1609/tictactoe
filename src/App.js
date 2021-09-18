b6206227ccad61931708ebe83b603486a72b1fbd
MEHUL BHARDWAJ <66671284+mehulbhardwaj1609@users.noreply.github.com>
2021-09-18 23:17:39 +0530

    displayed game history

--- src/App.js
+++ src/App.js
@@ -2,38 +2,52 @@ import React, { useState } from 'react';
 import Board from './components/Board';
 import { calculateWinner } from './helper';
 import './styles/root.scss';
+import History from './components/History';
 
 const App = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [isNext, setIsNext] = useState(false);
+  const [history, setHistory] = useState([
+    { board: Array(9).fill(null), isNext: true },
+  ]);
 
-  const winner = calculateWinner(board);
+  const [currentMove, setCurrentMove] = useState(0);
+
+  const current = history[currentMove];
+  const winner = calculateWinner(current.board);
 
   const message = winner
     ? `Winner is ${winner}`
-    : `Next player is ${isNext ? 'X' : 'O'}`;
+    : `Next player is ${current.isNext ? 'X' : 'O'}`;
 
   const handleSquareClick = position => {
-    if (board[position] || winner) return;
+    if (current.board[position] || winner) return;
 
-    setBoard(prev => {
-      return prev.map((square, pos) => {
+    setHistory(prev => {
+      const last = prev[prev.length - 1];
+
+      const newBoard = last.board.map((square, pos) => {
         if (pos === position) {
-          return isNext ? 'X' : 'O';
+          return last.isNext ? 'X' : 'O';
         }
 
         return square;
       });
+      return prev.concat({ board: newBoard, isNext: !last.isNext });
     });
-    setIsNext(prev => !prev);
+    setCurrentMove(prev => prev + 1);
+  };
+
+  const moveTo = move => {
+    setCurrentMove(move);
   };
 
   return (
     <div className="app">
       <h1>Tic Tac Toe</h1>
       <h2>{message}</h2>
-      <Board board={board} handleSquareClick={handleSquareClick} />
+      <Board board={current.board} handleSquareClick={handleSquareClick} />
+      <History history={history} moveTo={moveTo} currentmove={currentMove} />
     </div>
   );
 };
+
 export default App;
