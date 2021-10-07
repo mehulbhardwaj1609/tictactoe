1f73fbb177a4ab9d87a26d574be7dc81daa82f0a
MEHUL BHARDWAJ <66671284+mehulbhardwaj1609@users.noreply.github.com>
2021-10-07 21:53:03 +0530

    displayed draw messaged

--- src/App.js
+++ src/App.js
@@ -1,38 +1,39 @@
 import React, { useState } from 'react';
 import Board from './components/Board';
-import { calculateWinner } from './helper';
-import './styles/root.scss';
 import History from './components/History';
+import StatusMessage from './components/StatusMessage';
+import { calculateWinner } from './helper';
+
+import './styles/root.scss';
 
 const App = () => {
   const [history, setHistory] = useState([
-    { board: Array(9).fill(null), isNext: true },
+    { board: Array(9).fill(null), isXNext: true },
   ]);
-
   const [currentMove, setCurrentMove] = useState(0);
-
   const current = history[currentMove];
+
   const winner = calculateWinner(current.board);
 
-  const message = winner
-    ? `Winner is ${winner}`
-    : `Next player is ${current.isNext ? 'X' : 'O'}`;
-
   const handleSquareClick = position => {
-    if (current.board[position] || winner) return;
+    if (current.board[position] || winner) {
+      return;
+    }
 
     setHistory(prev => {
       const last = prev[prev.length - 1];
 
       const newBoard = last.board.map((square, pos) => {
         if (pos === position) {
-          return last.isNext ? 'X' : 'O';
+          return last.isXNext ? 'X' : 'O';
         }
 
         return square;
       });
-      return prev.concat({ board: newBoard, isNext: !last.isNext });
+
+      return prev.concat({ board: newBoard, isXNext: !last.isXNext });
     });
+
     setCurrentMove(prev => prev + 1);
   };
 
@@ -42,10 +43,10 @@ const App = () => {
 
   return (
     <div className="app">
-      <h1>Tic Tac Toe</h1>
-      <h2>{message}</h2>
+      <h1>TIC TAC TOE</h1>
+      <StatusMessage winner={winner} current={current} />
       <Board board={current.board} handleSquareClick={handleSquareClick} />
-      <History history={history} moveTo={moveTo} currentmove={currentMove} />
+      <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
 };
--- src/components/History.js
+++ src/components/History.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const History = ({ history, moveTo, currentmove }) => {
+const History = ({ history, moveTo, currentMove }) => {
   return (
     <ul>
       {history.map((_, move) => {
@@ -8,7 +8,7 @@ const History = ({ history, moveTo, currentmove }) => {
           <li key={move}>
             <button
               style={{
-                fontWeight: move === currentmove ? 'bold' : 'normal',
+                fontWeight: move === currentMove ? 'bold' : 'normal',
               }}
               type="button"
               onClick={() => {
